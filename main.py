6445b5e29cfc805f74edf44cc5466d5adbf3cbbd
Isaac Lyons <isaac@snowskeleton.net>
2025-06-25 21:29:54 -0400

    gunicorn support

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import threading
 
 from storage import init_db
+from routes import app
 from utils import check_secrets, periodic_messages
 
 
 threading.Thread(target=periodic_messages, daemon=True).start()
 
+check_secrets()
+init_db()
+
 if __name__ == "__main__":
-    check_secrets()
-    init_db()
-    from routes import app
     app.run(host="0.0.0.0", port=5001, debug=True)
